d22479f523039fc45b9bda3b8e0b8d6e255659ef
Lorne Prince <lorneAprince@gmail.com>
2023-05-25 19:25:16 -0400

    initial commit

---
+++ GitCloneRepoSampleSln/GitCloneRepoSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitCloneRepoSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
